57504e17bee1c2e4ac0a885fa497ff5eca349d76
nandhuraju <nandhuraju6@gmail.com>
2024-09-22 14:01:04 +0530

    l2 :fire:

---
+++ Obsidian Horizon L2/button.gd
@@ -0,0 +1,5 @@
+extends Button
+
+
+func _on_pressed() -> void:
+	OS.shell_open("https://market-place-theta-two.vercel.app") 
---
+++ Obsidian Horizon L2/stake.gd
@@ -0,0 +1,5 @@
+extends Button
+
+
+func _on_pressed() -> void:
+	OS.shell_open("https://obsidian-horizon-stake.vercel.app/") 
